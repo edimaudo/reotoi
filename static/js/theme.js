45d6386c18ca17d498d5fbb641e02f3d0a358b81
Edima <edimaudo@gmail.com>
2026-09-18 13:35:36 -0400

    Update theme.js

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,1 +1,10 @@
 
+(() => {
+  "use strict";
+
+  const page = document.querySelector(".gallery-page");
+  if (!page) return;
+
+  // Gallery persistence will be connected when the server-side gallery store is added.
+  // Keeping this file page-scoped avoids running gallery code on other views.
+})();
